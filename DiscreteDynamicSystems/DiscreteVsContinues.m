8cbf813e2b7fe85f8e31cbbfb36756a5da4a1888
Milosz Blasiak <milblasiak@gmail.com>
2018-12-20 19:32:56 +0100

    Add showcase of Continues-Discrete Sysmtem

---
+++ DiscreteDynamicSystems/DiscreteVsContinues.m
@@ -0,0 +1,75 @@
+clc;
+close all;
+clear;
+%Parameters with _c are used in continues State Space
+%Setting state equation
+%A is state matrix of our system and B is matrix of controll
+A=[1,1;1,1];
+A_c=A;
+%B=0 means system is autonomous
+B=[0;0];
+B_c=B;
+%Seting what y equals to
+%Using eye for C and [0 0] for D
+%We make [y1,y2]=[x1,x2]
+C=eye(2);
+C_c=C;
+%We dont have controll of the outpus so D=0
+D=[0;0];
+D_c=D;
+
+%Sampling time (Time between taking samples
+%from analog input and transforming to discrate signal 
+h=1e-2;
+
+
+%Start state of system
+Start=0;
+Start_c=Start;
+
+%Tesi is everyting working
+sim('model_2');
+plot(tout,x);
+
+%%
+%Task 2
+
+%My I N dependent on lengt of name and surename
+I=length('Milosz');
+N=length('Blasiak');
+
+%Applaying vaules to matrixes
+A_c=[0 1;-I -N];
+B_c=[0;1];
+C_c=[1 0; 0 1];
+D_c=[0;0];
+
+h=0.01;
+
+%Using equation given in file
+A=expm(h*A);
+%Define that z is symbolic
+syms z;
+%Int=Integral function in matlab from 0 to h using z(t) 
+%Cast intergration on double becouse function will return synbloic result
+B=double(int(expm(z*A_c)*B_c,z,0,h));
+C=C_c; 
+D=D_c;
+
+Start=5;
+Start_c=Start;
+sim('model_2');
+
+
+%!!!!!!!!!!!!!!!!!SPYTAC !!!!! CHYBA POWINNY MIEC TEN SAM PRZEBIEG PRZEZ
+%CAŁY CZAS
+
+figure(2);
+plot(tout,x(:,1),'red');
+hold on;
+plot(tout,x(:,2),'blue');
+title("Discrete-Continues Syste,");
+legend('Continues','Discrete');
+xlabel("time t");
+ylabel("x(t)");
+
