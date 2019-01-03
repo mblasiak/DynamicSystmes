f04acce501d6d4faa69b03acf761f1d7a9edcebf
Milosz Blasiak <milblasiak@gmail.com>
2019-01-03 18:58:24 +0100

    Add script chaecking stabbilty using nyquist

---
+++ FrequencyAnalisys/Stabillity.m
@@ -0,0 +1,34 @@
+%Clear all left overs
+clc;
+close all;
+clear;
+
+%Set system parameters
+%Nominator Dominator
+N=[0 1];
+D=[1 1 2];
+%Create Systme
+system=tf(N,D);
+
+figure;
+%Show nyquist characteristic
+nyquist(system);
+hold on;
+%Print empty point in (-1,0) so stabillity can be easilly deterimned
+%matlab will show plot including this point
+plot(-1,0,'');
+
+
+%Same with the second system
+
+N=[1 0];
+D=[1 -0.1 2];
+system=tf(N,D);
+
+figure;
+nyquist(system);
+hold on;
+plot(-1,0,'');
+
+%Detrimne stabillity of closed system by chkecking nyquist stabillity
+%when the system is open (has no feedback loop)
